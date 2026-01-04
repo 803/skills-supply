773e63ac32398e2ddbb2fa29c72f3854da0cb7d7
Alizain Feerasta <alizain.feerasta@gmail.com>
2026-01-03 20:40:59 -0500

    m

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from "vitest/config"
 
 export default defineConfig({
 	test: {
-		projects: ["packages/sk/vitest.config.ts"],
+		projects: [
+			"packages/core/vitest.config.ts",
+			"packages/discovery/vitest.config.ts",
+			"packages/sk/vitest.config.ts",
+		],
 	},
 })
