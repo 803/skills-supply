05b20f00eee42f2b89e5a286ab178a487f2ba9f2
Alizain Feerasta <alizain.feerasta@gmail.com>
2026-01-12 12:58:00 -0500

    style(npmrc): remove quotes around npm config scope names

--- npmrc.sh
+++ npmrc.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-npm config --location=project set '@fortawesome:registry https://npm.fontawesome.com'
-npm config --location=project set '@awesome.me:registry https://npm.fontawesome.com'
+npm config --location=project set @fortawesome:registry https://npm.fontawesome.com
+npm config --location=project set @awesome.me:registry https://npm.fontawesome.com
 npm config --location=project set -- '//npm.fontawesome.com/:_authToken=${FONTAWESOME_TOKEN}'
